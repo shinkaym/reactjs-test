40770c510e137f509f26dae33bed3c40bb87478f
shinkaym <khoapham1405@gmail.com>
2024-03-03 12:01:09 +0700

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ToDoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Khoa dep trai
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToDoFeature/>
     </div>
   );
 }
---
+++ src/features/Todo/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ToDoFeature.propTypes = {
+  
+};
+
+function ToDoFeature(props) {
+  const todoList = [
+    {
+      id: 1,
+      title: 'Eat'
+    },
+    {
+      id: 2,
+      title: 'Sleep'
+    },
+    {
+      id: 3,
+      title: 'Code'
+    }
+  ]
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ToDoFeature;
